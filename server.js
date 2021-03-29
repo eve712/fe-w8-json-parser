e0d94080bb4909f9670b3e110bbff07842f0ac84
eve712 <62237639+eve712@users.noreply.github.com>
2021-03-30 00:44:41 +0900

    Add: 웹팩 미들웨어 설치

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const express = require("express");
 const path = require("path");
 const server = express();
 
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
+const compiler = webpack(webpackConfig);
+
+server.use(webpackDevMiddleware(compiler, {
+  publicPath: webpackConfig.output.publicPath,
+}))
+
 server.set("port", 3000);
 
-server.use(express.static(path.join(__dirname, "public")));
-
-// server.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname + "/public/index.html"));
-// });
-
-server.get("/index.bundle.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/public/index.bundle.js"));
-});
-server.get("/index.bundle.js.map", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/public/index.bundle.js.map"));
+server.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
 });
 
 server.listen(server.get("port"), () => {
