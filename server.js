2bb4411d47d735d72c4b489922c7336daf8658f2
dudn1933 <75162982+dudn1933@users.noreply.github.com>
2021-03-29 23:08:02 +0900

    ADD html-webpack-plugin

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const path = require('path')
+const express = require("express");
+const path = require("path");
 const server = express();
 
-server.set('port', 3000);
+server.set("port", 3000);
 
-server.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/public/index.html'))
-}); 
-server.get('/index.bundle.js', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/public/index.bundle.js'))
-})
-server.get('/index.bundle.js.map', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/public/index.bundle.js.map'))
-})
+server.use(express.static(path.join(__dirname, "public")));
 
-server.listen(server.get('port'), () => {
-    console.log(server.get('port'), '번 포트에서 대기 중')
-})
+// server.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname + "/public/index.html"));
+// });
+
+server.get("/index.bundle.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/public/index.bundle.js"));
+});
+server.get("/index.bundle.js.map", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/public/index.bundle.js.map"));
+});
+
+server.listen(server.get("port"), () => {
+  console.log("http://localhost:" + server.get("port"));
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,40 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    mode: "development",
-    devtool: 'source-map',
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname + "public"),
-        filename: 'index.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        "presets": ["@babel/preset-env"],
-                    }
-                }
-            },
-            {
-                test:/\.(sa|sc|c)ss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'public'), 
-        compress: true,
-        port: 9000,
-    },
-}
+  mode: "development",
+  devtool: "source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname + "/public"),
+    filename: "index.bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+};
