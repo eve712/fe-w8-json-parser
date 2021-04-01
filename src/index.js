e100c08b7f9136b34b6cdbc3915f4377fe32116a
eve712 <62237639+eve712@users.noreply.github.com>
2021-04-01 16:47:22 +0900

    Add: array, object 나누고, type 지정

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Tokenizer from './tokenizer.js';
 import Lexer from './lexer.js';
 import Parser from './parser.js';
 
-const input = '[false, {"[eve]": 21}, [23, null]]'
+const input = '[false, [23, null]]'
 
 const tokenizer = new Tokenizer();
 const lexer = new Lexer();
